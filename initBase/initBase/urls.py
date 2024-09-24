a2c3ebadf2011f2daf5362122ba5cfa816128055
param <75372705+dev-param@users.noreply.github.com>
2024-09-24 15:59:18 +0530

    new setup

--- initBase/initBase/urls.py
+++ initBase/initBase/urls.py
@@ -15,18 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from userManagement import userManagementUrls
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include(userManagementUrls.urlpatterns))
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
-        
-
-  
--- initBase/userapi/apps.py
+++ initBase/userapi/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsermanagementConfig(AppConfig):
+class UserapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userManagement'
+    name = 'userapi'
